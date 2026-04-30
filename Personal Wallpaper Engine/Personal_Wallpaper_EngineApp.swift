6404e0fc3ef5501e5ad674c25fefee06b0b7ab94
Arnav Aggarwal <arnavaggarwal1@gmail.com>
2026-04-30 16:31:50 -0700

    Initial Commit

---
+++ Personal Wallpaper Engine/Personal_Wallpaper_EngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Wallpaper_EngineApp.swift
+//  Personal Wallpaper Engine
+//
+//  Created by Arnav Aggarwal on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Wallpaper_EngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
